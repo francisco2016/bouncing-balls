ca747a92834e8de1567f4b690ecfbeff4079960e
francisco2016 <otrayama@gmail.com>
2016-03-31 11:42:40 +0200

    0100 se crean tantas bolas como el usuario quiera.

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.ArrayList; //-------------------------------------------------- 0100
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -28,26 +29,32 @@ public class BallDemo
         int ground = 400;   // position of the ground line
 
         myCanvas.setVisible(true);
-
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
-
+        //---0100 creamos una coleccion de objetos BouncingBall() que llamamos bolas.//-----------------------0100
+        ArrayList<BouncingBall> bolas = new ArrayList<>(); //-------------------------------------------------0100
+        int posicion = 50;
         // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
+        //creamos un bucle para ir asignando bolas a la colección. -------------------------------------------0100
 
-        // make them bounce
+        for(int i = 0; i < numBolas; i++){//-----------------------------------------------------------------0100
+            bolas.add(new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas));
+            bolas.get(i).draw();
+        }
+
+        // make them bounce ----------------( que boten  )
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            //para que boten las bolas creamos un bucle for each que recorra toda la colección y haga moverse a cada bola.0100
+            for(BouncingBall bola : bolas){//-----------------------------------------------------------------0100
+                bola.move();
+
+                if(bola.getXPosition() >= 550  ) {
+                    finished = true;//-----------------------------------------------------------------------0100
+                }
             }
+          // stop once ball has travelled a certain distance on x axis
         }
     }
 }
